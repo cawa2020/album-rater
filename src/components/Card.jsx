90c48dfee4e8f420594336fbabe7fe0bea99927f
cawa <you@example.com>
2024-09-02 17:23:46 +0400

    100

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div className="card">
+      <div className="card-header">
+        <img src="" alt="" />
+        <h1 className="mt-10">название</h1>
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+    </div>
+  );
+}
